318d4c68b03f717de163055b372370096a6f9d04
mac7iek <161622341+mac7iek@users.noreply.github.com>
2024-03-13 16:36:15 +0000

    c4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,11 @@
 
 public class Main {
   public static void main(String[] args) {
-  int sum = 0;
-    for(int i = 1; i <=100; i+=2){
-      sum += i;
+  System.out.println("stopnie Celcjusza\tStopnie Fahrenheita");
+    for(int C = 0; C <= 300; C += 20) {
+      double F = (C * 9.0 / 5.0) + 32.0;
+      System.out.printf(" %d\t\t\t\t\t%.2f\n", C, F);
     }
-    System.out.println(sum);
-}
+    }
 }
 //....
