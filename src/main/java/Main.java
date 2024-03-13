69d7aac9d93c181fdbdef60ccb5d3c94a7445ff1
mac7iek <161622341+mac7iek@users.noreply.github.com>
2024-03-13 15:58:01 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    String a = "helloWorld";
+
+     for(int i = 0; i < a.length(); i++){
+       for(int j = 0; j < i; j++){
+         System.out.print(" ");
+       }
+      System.out.println(a.charAt(i));
+    }
+}
+ 
+}
